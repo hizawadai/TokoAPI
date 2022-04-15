e6a5f9ea6f2bed6e8e8b6156c100dfcd46616c8e
HzamaDai <danihizawa@gmail.com>
2022-04-15 14:17:05 +0700

    Register

---
+++ app/Controllers/RegistrasiController.php
@@ -0,0 +1,36 @@
+<?php
+
+ namespace App\Controllers;
+
+ use App\Models\MRegistrasi;
+ use CodeIgniter\RESTful\ResourceController;
+
+ class RegistrasiController extends ResourceController
+ {
+    protected function responseHasil($code, $status, $data)
+    {
+        return $this->respond([
+            'code' => $code,
+            'status' => $status,
+            'data' => $data,
+        ]);
+    }
+    public function index()
+    {
+        $model = new MRegistrasi ();
+        $member = $model -> findAll();
+        return $this -> ResponseHasil (200,true,$member);
+    }
+
+    public function registrasi()
+    {
+        $data = [
+            'nama' => $this->request->getVar('nama'),
+            'email' => $this->request->getVar('email'),
+            'password' => password_hash($this->request->getVar('password'),PASSWORD_DEFAULT)
+        ];
+        $model = new MRegistrasi();
+        $model->save($data);
+        return $this->responseHasil(200, true,"Registrasi Berhasil");
+    }
+}
---
+++ app/Models/MRegistrasi.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App\Models;
+    use CodeIgniter\Model;
+
+    class MRegistrasi extends Model
+    {
+    protected $table = ' member ';
+    protected $PrimaryKey ='id';
+    protected $allowedFields = ['nama', 'email', 'password'];
+    }
